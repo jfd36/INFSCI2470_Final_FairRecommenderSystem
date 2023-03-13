c8933e114967b142ab4e210ceb733dafe7dda10d
Quinn K Wolter <QuinnKWolter@gmail.com>
2023-03-13 18:54:45 -0400

    Hopefully fix bizarre script issue. It only executes correctly when I leave the comment at the bottom; otherwise it fails to iterate...? Wasted too much time on this. Also, I made the Admin display for everything prettier.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Genre(models.Model):
     title = models.CharField(max_length=300)
 
+    def __str__(self):
+        return f"{self.title}"
+
 class Movie(models.Model):
     movieId = models.IntegerField()
     title = models.CharField(max_length=300)
@@ -12,11 +15,17 @@ class Movie(models.Model):
     tmdbId = models.CharField(max_length=10)
     genres = models.ManyToManyField(Genre)
 
+    def __str__(self):
+        return f"{self.title} ({self.year})"
+
 class Tag(models.Model):
     userId = models.IntegerField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     tag = models.CharField(max_length=300)
     timestamp = models.DateTimeField(max_length=50)
+
+    def __str__(self):
+        return f"User {self.userId} tagged {self.movie} as: {self.tag}"
 
 class Rating(models.Model):
     userId = models.IntegerField()
@@ -24,11 +33,20 @@ class Rating(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     timestamp = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"User {self.userId} rated {self.movie} as: {self.rating}"
+
 class Genome_Score(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     tagId = models.IntegerField()
     relevance = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return f"Movie {self.movie} scores {self.relevance} for: {self.tagId}"
+
 class Genome_Tag(models.Model):
     tagId = models.IntegerField()
     tag = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"tagId {self.tagId} ({self.tag})"
--- data script.py
+++ data script.py
@@ -3,84 +3,86 @@ import re
 from datetime import datetime, timezone
 from app.models import *
 
-# create Genre table
-df_movies = pd.read_csv('movies.csv')
-genre_set = set()
-for index, row in df_movies.iterrows():
-    for genre in row['genres'].split("|"):
-        genre_set.add(genre)
+# # create Genre table
+# df_movies = pd.read_csv('movies.csv')
+# genre_set = set()
+# for index, row in df_movies.iterrows():
+#     for genre in row['genres'].split("|"):
+#         genre_set.add(genre)
 
-print("GENRES TIME")
-for genre in genre_set:
-    obj = Genre()
-    obj.title = genre
-    obj.save()
+# print("GENRES TIME")
+# for genre in genre_set:
+#     obj = Genre()
+#     obj.title = genre
+#     obj.save()
 
-print("MOVIES TIME")
-# create Movie table 
-for index, row in df_movies.iterrows():
-    obj = Movie()
-    obj.movieId = row['movieId']
-    match = re.search(r'\((\d{4})\)$', row['title'])
-    if match:
-        year = match.group(1)
-        title = re.sub(r'\s*\(\d{4}\)$', '', row['title'])
-    obj.title = title
-    obj.year = year
-    obj.save()
+# print("MOVIES TIME")
+# # create Movie table 
+# for index, row in df_movies.iterrows():
+#     obj = Movie()
+#     obj.movieId = row['movieId']
+#     match = re.search(r'\((\d{4})\)$', row['title'])
+#     if match:
+#         year = match.group(1)
+#         title = re.sub(r'\s*\(\d{4}\)$', '', row['title'])
+#     obj.title = title
+#     obj.year = year
+#     obj.save()
 
-print("GENRES TO MOVIES TIME")
-for index, row in df_movies.iterrows():
-    obj = Movie.objects.get(movieId=row['movieId'])
-    for genre in row['genres'].split("|"):
-        obj.genres.add(Genre.objects.get(title=genre))
+# print("GENRES TO MOVIES TIME")
+# for index, row in df_movies.iterrows():
+#     obj = Movie.objects.get(movieId=row['movieId'])
+#     for genre in row['genres'].split("|"):
+#         obj.genres.add(Genre.objects.get(title=genre))
 
-# add links to Movie table (connects movies with their IMDB and TMDB links)
-print("LINKS TIME")
-df_links = pd.read_csv('links.csv', dtype=str)
-for index, row in df_links.iterrows():
-    obj = Movie.objects.get(movieId=row['movieId'])
-    obj.imdbId = row['imdbId']
-    obj.tmdbId = row['tmdbId']
-    obj.save()
+# # add links to Movie table (connects movies with their IMDB and TMDB links)
+# print("LINKS TIME")
+# df_links = pd.read_csv('links.csv', dtype=str)
+# for index, row in df_links.iterrows():
+#     obj = Movie.objects.get(movieId=row['movieId'])
+#     obj.imdbId = row['imdbId']
+#     obj.tmdbId = row['tmdbId']
+#     obj.save()
 
-# create Tag table
-print("TAGS TIME")
-df_tags = pd.read_csv('tags.csv')
-for index, row in df_tags.iterrows():
-    obj = Tag()
-    obj.movie = Movie.objects.get(movieId=row['movieId'])
-    obj.userId = row['userId']
-    obj.tag = row['tag']
-    obj.timestamp = datetime.fromtimestamp(row['timestamp'], timezone.utc)
-    obj.save()
+# # create Tag table
+# print("TAGS TIME")
+# df_tags = pd.read_csv('tags.csv')
+# for index, row in df_tags.iterrows():
+#     obj = Tag()
+#     obj.movie = Movie.objects.get(movieId=row['movieId'])
+#     obj.userId = row['userId']
+#     obj.tag = row['tag']
+#     obj.timestamp = datetime.fromtimestamp(row['timestamp'], timezone.utc)
+#     obj.save()
 
-print("GENOME SCORES TIME")
-# create Genome_Score table
-df_genome_scores = pd.read_csv('genome-scores.csv')
-for index, row in df_genome_scores.iterrows():
-    obj = Genome_Score()
-    obj.movie = Movie.objects.get(movieId=row['movieId'])
-    obj.tagId = row['tagId']
-    obj.relevance = row['relevance']
-    obj.save()
+# print("GENOME SCORES TIME")
+# # create Genome_Score table
+# df_genome_scores = pd.read_csv('genome-scores.csv')
+# for index, row in df_genome_scores.iterrows():
+#     obj = Genome_Score()
+#     obj.movie = Movie.objects.get(movieId=row['movieId'])
+#     obj.tagId = row['tagId']
+#     obj.relevance = row['relevance']
+#     obj.save()
 
-print("GENOME TAGS TIME")
-# create Genome_Tag table
-df_genome_tags = pd.read_csv('genome-tags.csv')
-for index, row in df_genome_tags.iterrows():
-    obj = Genome_Tag()
-    obj.tagId = row['tagId']
-    obj.tag = row['tag']
-    obj.save()
+# print("GENOME TAGS TIME")
+# # create Genome_Tag table
+# df_genome_tags = pd.read_csv('genome-tags.csv')
+# for index, row in df_genome_tags.iterrows():
+#     obj = Genome_Tag()
+#     obj.tagId = row['tagId']
+#     obj.tag = row['tag']
+#     obj.save()
 
 print("RATINGS TIME")
-# create Rating table
-df_ratings = pd.read_csv('ratings.csv')
-for index, row in df_ratings.iterrows():
+# create Rating table (note from Quinn: Had to adjust this, as for some reason it was misbehaving...?)
+df_ratings = pd.read_csv('ratings.csv', dtype=str)
+for row in df_ratings.values:
     obj = Rating()
-    obj.userId = row['userId']
-    obj.movie = Movie.objects.get(movieId=row['movieId'])
-    obj.rating = row['rating']
-    obj.timestamp = datetime.fromtimestamp(row['timestamp'], timezone.utc)
+    obj.userId = row[0]
+    obj.movie = Movie.objects.get(movieId=row[1])
+    obj.rating = row[2]
+    obj.timestamp = datetime.fromtimestamp(int(row[3]), timezone.utc)
     obj.save()
+
+# for index, row in df_ratings.iterrows():
