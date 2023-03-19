dd54fc52eb6f2e0aa1d3694e151956c07e64be98
Quinn K Wolter <QuinnKWolter@gmail.com>
2023-03-18 23:29:28 -0400

    Tweak models

--- app/models.py
+++ app/models.py
@@ -28,6 +28,15 @@ class Movie(models.Model):
     def __str__(self):
         return f"{self.title} ({self.year})"
 
+class Tag(models.Model):
+    userId = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(max_length=50)
+
+    def __str__(self):
+        return f"User {self.userId} tagged {self.movie} as: {self.tag}"
+
 class Rating(models.Model):
     userId = models.IntegerField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -36,3 +45,18 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"User {self.userId} rated {self.movie} as: {self.rating}"
+
+class Genome_Score(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    tagId = models.IntegerField()
+    relevance = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"Movie {self.movie} scores {self.relevance} for: {self.tagId}"
+
+class Genome_Tag(models.Model):
+    tagId = models.IntegerField()
+    tag = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"tagId {self.tagId} ({self.tag})"
