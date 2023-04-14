b3bec36ed1ff0ea880b146f658dd1300b81379c2
Quinn K Wolter <QuinnKWolter@gmail.com>
2023-04-13 20:30:01 -0400

    Standardize files, push for team

--- static/js/example.js
+++ static/js/example.js
@@ -22,9 +22,7 @@ $(document).ready(function() {
         const random = d3.randomNormal(0, 0.2);
         const sqrt3 = Math.sqrt(3);
         return [].concat(
-          Array.from({ length: 300 }, () => [random() + sqrt3, random() + 1, 0]),
-          Array.from({ length: 300 }, () => [random() - sqrt3, random() + 1, 1]),
-          Array.from({ length: 300 }, () => [random(), random() - 1, 2]),
+          Array.from({ length: 900 }, () => [random() + sqrt3, random() + 1, 0]),
         //   'TEST DATA'
         );
       })();
